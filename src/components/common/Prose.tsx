3d984c5183e68b669368f57a2878b9f8126acc3a
Abiola Awoyemi <cipherx809@gmail.com>
2026-05-25 17:10:54 +0100

    feat: add completed and completion note props to prose for activity checklist

--- src/components/common/Prose.tsx
+++ src/components/common/Prose.tsx
@@ -1,13 +1,24 @@
 export const Prose = ({
   info,
 }: {
-  info: { title: string; content: string }[];
+  info: {
+    title: string;
+    content: string;
+    completed?: boolean;
+    footnote?: string;
+  }[];
 }) => {
   return (
     <ul className="w-full text-fg-dim pl-4.5 my-2 mb-3.5 list-disc">
       {info.map((item, index) => (
-        <li className="my-1 max-w-3xl" key={index}>
-          <strong>{item.title}</strong> — {item.content}
+        <li className="my-2 max-w-3xl" key={index}>
+          <strong>{item.title}</strong> —{" "}
+          <span className={item.completed ? "line-through" : ""}>
+            {item.content}
+          </span>
+          {item.footnote && (
+            <span className="block text-xs">{item.footnote}</span>
+          )}
         </li>
       ))}
     </ul>
